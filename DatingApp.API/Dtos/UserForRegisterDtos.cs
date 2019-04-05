992bd6552d0f6ce1fbe7b506ead75b5d7eef0a9e
jasonpaulchris <jasonpaulchristensen@gmail.com>
2019-04-05 12:13:21 -0500

    added security

---
+++ DatingApp.API/Dtos/UserForRegisterDtos.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDtos
+    {  
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
